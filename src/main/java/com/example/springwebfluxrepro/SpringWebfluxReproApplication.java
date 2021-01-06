379d6854a7db3766e6aa6bd58b1795870ccc4cf6
jasonjkeller <jasonjkeller@gmail.com>
2021-01-06 12:05:47 -0800

    Repro missing external call with Mono just

--- src/main/java/com/example/springwebfluxrepro/SpringWebfluxReproApplication.java
+++ src/main/java/com/example/springwebfluxrepro/SpringWebfluxReproApplication.java
@@ -2,11 +2,15 @@ package com.example.springwebfluxrepro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class SpringWebfluxReproApplication {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();
+        // Use it only if you see that some call-sites are not instrumented
+        ReactorDebugAgent.processExistingClasses();
         SpringApplication.run(SpringWebfluxReproApplication.class, args);
     }
 
--- src/main/java/com/example/springwebfluxrepro/controllers/WebFluxReproController.java
+++ src/main/java/com/example/springwebfluxrepro/controllers/WebFluxReproController.java
@@ -1,5 +1,7 @@
 package com.example.springwebfluxrepro.controllers;
 
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Token;
 import com.newrelic.api.agent.Trace;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -18,9 +23,9 @@ import java.util.List;
 @RestController
 public class WebFluxReproController {
 
-    private final WebClient serviceClient = WebClient.create();
     private static final ParameterizedTypeReference<List<String>> LIST_OF_STRING_TYPE_REF = new ParameterizedTypeReference<>() {
     };
+    private final WebClient serviceClient = WebClient.create();
 
     @GetMapping("/external/flux/flatMap")
     public Flux<String> getExternalsWithReturnTypeFlux() {
@@ -65,4 +70,35 @@ public class WebFluxReproController {
                 .retrieve()
                 .bodyToMono(String.class);
     }
+
+    @Trace
+    public Mono<String> postExternalMonoAlternative(String value) {
+        Mono<String> stringMono = serviceClient
+                .post()
+                .uri("http://example.net")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(value)
+                .retrieve()
+                .bodyToMono(String.class);
+        return stringMono;
+    }
+
+    @GetMapping("/fireAndForget")
+    public Mono<String> callWithFireAndForget() {
+        return this.postExternalMono("foo")
+                .doOnNext(this::fireAndForgetLogic); // doOnNext splits work off to separate thread and hits switchIfEmpty
+    }
+
+    @Trace(async = true)
+    private void fireAndForgetLogic(final String tests) {
+        Token token = NewRelic.getAgent().getTransaction().getToken();
+        Mono.just(tests)
+                .flatMap(this::postExternalMonoAlternative) // ---> NOT TRACKED external call
+                .delayElement(Duration.ofSeconds(1))
+                .subscribeOn(Schedulers.parallel())
+                .subscriberContext(Context.of("nr-token", token))
+                .doOnNext(s -> token.link())
+                .subscribe(); // subscribe splits work off to separate thread and hits switchIfEmpty
+    }
+
 }
